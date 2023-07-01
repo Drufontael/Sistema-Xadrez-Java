6c36326e1efd762b4aeb587a584e85766c28acdc
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-07-01 10:03:58 -0300

    Chess layer e imprimir tabuleiro

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,13 @@
 package aplication;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Position pos=new Position(4,5);
-        System.out.println(pos);
+        ChessMatch chessMatch=new ChessMatch();
+        UI.printBoard(chessMatch.getPiece());
     }
+
+
 }
