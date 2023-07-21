150af7d06204d58d52e5da64d99d32d2a7e532f2
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-07-21 16:42:25 -0300

    Update Program.java

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
         Scanner sc=new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
